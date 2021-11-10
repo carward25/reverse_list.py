8db2c320eb603749723fd49b2f5d64007f48aed8
carward25 <carward@ucsc.edu>
2021-11-10 11:53:37 -0800

    Initial commit

---
+++ reverse_list.py
@@ -0,0 +1,13 @@
+#name: Cassidy Ward
+#date:11/10/2021
+#description: this program takes as a parameter a list and and reverses the order of the elements in that list
+
+def reverse_list(l):
+    i = 0
+    j = len(lst)-1
+    while (i<j):
+        tmp = lst[i]
+        lst[i] = lst[j]
+        lst[j] = tmp
+        i+=1
+        j-=1
